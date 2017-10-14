84e017e57f489a0e76f19af63c4fb9a1fbd82e8d
Alex Mueller <alex.mueller@ambiac.de>
2017-10-14 16:37:47 +0200

    fix: install script failing on non-existing .git/hooks path

--- misc/validate-commit.install.js
+++ misc/validate-commit.install.js
@@ -4,9 +4,24 @@ var fs = require('fs');
 var path = require('path');
 
 var hookInputPath = 'misc/validate-commit.js';
-var hookOutputPath = '.git/hooks/commit-msg';
+var hookOutputPath = '.git/hooks';
+var hookOutputFile = hookOutputPath + '/commit-msg';
+var mode = '0755';
 
-if (!fs.existsSync(hookOutputPath)) {
-  fs.writeFileSync(hookOutputPath, fs.readFileSync(hookInputPath));
-  fs.chmodSync(hookOutputPath, '0755');
+function mkdirRecursive(fs, dir) {
+  var current = '';
+
+  dir.split('/').forEach(function(path) {
+    current += (current.length > 0  ? '/' : '') + path;
+
+    if ( !fs.existsSync(current) ) {
+      fs.mkdirSync(current, mode);
+    }
+  });
+}
+
+if (!fs.existsSync(hookOutputFile)) {
+  mkdirRecursive(fs, hookOutputPath);
+  fs.writeFileSync(hookOutputFile, fs.readFileSync(hookInputPath));
+  fs.chmodSync(hookOutputFile, mode);
 }
